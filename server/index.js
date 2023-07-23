abd687daf17b89ad5829a2d55884020788a0dcb6
Gargi Singh <gargi_singh@icloud.com>
2023-07-24 02:01:01 +0530

    24/07

--- server/index.js
+++ server/index.js
@@ -29,5 +29,15 @@ app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
-
+//MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;   //helps access environment variables of .env file
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+}) 
+.then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+})
+. catch((error ) => console.log(`${error} did not connect`))
 
