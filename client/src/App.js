9d53089b258103ffd8328550eea624b1d35c6885
Gargi Singh <gargi_singh@icloud.com>
2023-07-26 23:56:01 +0530

    26/07

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "theme";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+  const mode = useSelector((state) => state.global.mode);   //add dark mode from state folder
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return <div className="app"></div>
+  
 }
 
 export default App;
